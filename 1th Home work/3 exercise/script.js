2a9b7013fbe1a618fb64d16c2d3c4af536cd43d6
Oleh Yushchuk <oleh.yushchuk@gmail.com>
2023-03-15 00:40:58 +0200

    Add Task 3

---
+++ 1th Home work/3 exercise/script.js
@@ -0,0 +1,4 @@
+let km = prompt("What is the distance between two cities?");
+let time = prompt("What is the time between two cities?");
+let result = km/time;
+alert(`It is the speed need to arrive in the time!: ${result}km/h`);
