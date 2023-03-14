ca70171b81b39cda1896631e69f9cec27b925a37
Oleh Yushchuk <oleh.yushchuk@gmail.com>
2023-03-15 01:05:22 +0200

    Did task without cicle

--- 1th Home work/5 exercise/script.js
+++ 1th Home work/5 exercise/script.js
@@ -1,12 +1,7 @@
 let gb = prompt("How many GB in your Flash drive?");
 gb*=1024;
 let mb = 820;
-let result;
-let counter = 0;
+let result = Math.floor(gb/mb);
 
-while(gb >= mb){
-    gb -= mb; 
-    counter++;
-}
-alert(`We can saved ${counter} files of size 820 MB`);
+alert(`We can saved ${result} files of size 820 MB`);
 
